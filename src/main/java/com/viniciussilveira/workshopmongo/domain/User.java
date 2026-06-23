4e88b5b2cc06aba76a01c4ca647f0959b5a8160a
Vinicius Silveira <Vinisilveira.vianna@gmail.com>
2026-06-23 10:38:06 -0300

    Conectando ao MongoDB com Repository e Service

--- src/main/java/com/viniciussilveira/workshopmongo/domain/User.java
+++ src/main/java/com/viniciussilveira/workshopmongo/domain/User.java
@@ -3,27 +3,33 @@ package com.viniciussilveira.workshopmongo.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection="user")
 public class User implements Serializable {
-	static final long serialVersionUID = 1L;
-	private int id;
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	private String id;
 	private String name;
 	private String email;
 
 	public User() {
 	}
 
-	public User(int id, String name, String email) {
+	public User(String id, String name, String email) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.email = email;
 	}
 
-	public int getId() {
+	public String getId() {
 		return id;
 	}
 
-	public void setId(int id) {
+	public void setId(String id) {
 		this.id = id;
 	}
 
