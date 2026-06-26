962e7f934ba75a5b7851f17c40239c38ba13144c
Vinicius Silveira <Vinisilveira.vianna@gmail.com>
2026-06-25 23:09:57 -0300

    Endpoit posts de um user

--- src/main/java/com/viniciussilveira/workshopmongo/resources/UserResource.java
+++ src/main/java/com/viniciussilveira/workshopmongo/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.viniciussilveira.workshopmongo.domain.Post;
 import com.viniciussilveira.workshopmongo.domain.User;
 import com.viniciussilveira.workshopmongo.dto.UserDTO;
 import com.viniciussilveira.workshopmongo.services.UserService;
@@ -60,6 +61,10 @@ public class UserResource {
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 	}
-	
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
+	}
 	
 }
